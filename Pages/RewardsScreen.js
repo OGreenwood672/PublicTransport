0906954b3e1dbabdb799fe4fa5177d02edf749ed
Oliver Greenwood <greenwood672.dev@gmail.com>
2024-03-17 06:14:40 +0000

    Added more rewards

--- Pages/RewardsScreen.js
+++ Pages/RewardsScreen.js
@@ -10,6 +10,15 @@ const images = {
     "1": require("../assets/1.png"),
     "2": require("../assets/2.png"),
     "3": require("../assets/3.png"),
+    "4": require("../assets/4.png"),
+    "5": require("../assets/5.png"),
+    "6": require("../assets/6.png"),
+    "7": require("../assets/7.png"),
+    "8": require("../assets/8.png"),
+    "9": require("../assets/9.png"),
+    "10": require("../assets/10.png"),
+    "11": require("../assets/11.png"),
+    "12": require("../assets/12.png"),
 }
 
 const styles = StyleSheet.create({
@@ -108,7 +117,7 @@ function getRewards(category, name, walletAddress, navigation) {
 
 export default function RewardsScreen({ route, navigation }) {
 
-    const [tab, setTab] = useState("Merch");
+    const [tab, setTab] = useState("All");
 
     function getTabBar() {
 
